e4c9714d83819bc960e89ef7bdf381ec4c84c754
John Otander <johnotander@gmail.com>
2014-10-09 15:39:46 -0600

    Add more documentation, complete throttle server implementation.

--- README.md
+++ README.md
@@ -2,7 +2,49 @@
 
 Gulp plugin to test websites for slow connections by throttling your stream.
 
+## Installation
+
+```
+npm install --save-dev gulp-throttle
+```
+
+## Usage
+
+You can create a custom task in your gulpfile like so:
+
+`gulpfile.js`
+```js
+var gulp     = require('gulp'),
+    connect  = require('gulp-connect'),
+    throttle = require('gulp-throttle');
+
+gulp.task('throttle', function() {
+  connect.server();
+
+  throttle({
+    local_port: '8081',
+    remote_port: '8080',
+    upstream: 2000,
+    downstream: 2000,
+    keep_alive: true });
+});
+```
+
 ## Acknowledgements
 
 Adapted for the gulp build ecosystem from [grunt-throttle](https://github.com/tjgq/grunt-throttle)
 by Tiago Quelhas.
+
+## License
+
+MIT
+
+## Contributing
+
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
+
+Crafted with <3 by [John Otander](http://johnotander.com).
--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 
 var gutil = require('gulp-util'),
     net = require('net'),
-    throttleGroup = require('stream-throttle').ThrottleGroup;
+    ThrottleGroup = require('stream-throttle').ThrottleGroup;
 
 var defaults = {
   localhost: '127.0.0.1',
@@ -19,17 +19,21 @@ var defaults = {
 };
 
 module.exports = function(opts) {
+  gutil.log('Throttling...');
+
   opts = opts || {};
   opts.localhost  = opts.localhost || defaults.localhost;
   opts.remotehost = opts.remotehost || defaults.remotehost;
   opts.upstream   = opts.upstream || defaults.upstream;
-  opts.donwstream = opts.downstream || defaults.downstream;
+  opts.downstream = opts.downstream || defaults.downstream;
   opts.keep_alive = opts.keepalive || defaults.keepalive;
 
   var upThrottle = new ThrottleGroup({ rate: opts.upstream });
   var downThrottle = new ThrottleGroup({ rate: opts.downstream });
 
   var server = net.createServer({ allowHalfOpen: true }, function(local) {
+    gutil.log('Creating server for throttling...');
+
     var remote = net.createConnection({
       host: opts.remote_host,
       port: opts.remote_port,
@@ -41,25 +45,29 @@ module.exports = function(opts) {
 
     local.pipe(localThrottle).pipe(remote);
     local.on('error', function() {
+      gutil.log('Local server errored.');
       local.destroy();
       remote.destroy();
     });
 
-    server.listen(opts.local_port, opts.localhost);
+    remote.pipe(remoteThrottle).pipe(local);
+    remote.on('error', function() {
+      gutil.log('Remote server errored.');
+      local.destroy();
+      remote.destroy();
+    })
+  });
 
-    server.on('listening', function() {
-      var localAddress = opts.localhost + ':' + opts.local_port;
-      var remoteAddress = opts.remotehost + ':' + opts.remote_port;
-      gutil.log('Throttling connections to ' + remoteAddress + ', go to '+ localAddress);
-    });
+  server.listen(opts.local_port, opts.localhost);
 
-    server.on('error', function(err) {
-      var err = new gutil.PluginError('gulp-throttle', 'server failed');
-    });
+  server.on('listening', function() {
+    var localAddress = opts.localhost + ':' + opts.local_port;
+    var remoteAddress = opts.remotehost + ':' + opts.remote_port;
+    gutil.log('Throttling connections to ' + remoteAddress + ', go to '+ localAddress);
+  });
 
-    var done = this.async();
-    if (!opts.keep_alive) {
-      done();
-    }
+  server.on('error', function(err) {
+    gutil.log('Throttling server errored.');
+    var err = new gutil.PluginError('gulp-throttle', 'server failed');
   });
 };
